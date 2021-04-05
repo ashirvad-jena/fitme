80db7c402e4486d96d5d1483c15491dd2d8e92c7
Ashirvad Jena <>
2021-04-05 21:50:15 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+	componentDidMount() {
+		console.log("hello");
+		console.log("before");
+		console.log("after");
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your pp!</Text>
+				<StatusBar style="auto" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
